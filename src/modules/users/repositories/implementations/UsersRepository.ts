a9eb19a2c030542fbca1ecc050e2fea4132f104e
Ericlys Moreira <ericlysm@gmail.com>
2022-09-18 22:28:21 -0300

    feat: ending queries

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,31 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOneOrFail({
+      where: {id: user_id},
+      relations: ['games']
+    })
+
+    return user; 
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      select * 
+      from users 
+      order by first_name asc
+    `); 
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      select * 
+      from users u
+      where 
+      lower(u.first_name) = lower($1) and lower(u.last_name) = lower($2)`,
+      [first_name, last_name]); 
   }
 }
